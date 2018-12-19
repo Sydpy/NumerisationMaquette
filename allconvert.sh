05345c6825cb253a1900749171f583dbef196126
Remi GASCOU <remi@gascou.net>
2018-12-19 21:24:39 +0100

    Ajout des maquettes 4IRSCS1 4IRSCS2 4IRSIS1 4IRSIS2

---
+++ allconvert.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+/bin/bash convert.sh xml/maquettes/maquette_4IR_SI_S1.xml outputs_html/maquette_4IR_SI_S1.html
+/bin/bash convert.sh xml/maquettes/maquette_4IR_SI_S2.xml outputs_html/maquette_4IR_SI_S2.html
+/bin/bash convert.sh xml/maquettes/maquette_4IR_SC_S1.xml outputs_html/maquette_4IR_SC_S1.html
+/bin/bash convert.sh xml/maquettes/maquette_4IR_SC_S2.xml outputs_html/maquette_4IR_SC_S2.html
+/bin/bash convert.sh xml/maquettes/maquetteSDBD1.xml outputs_html/maquetteSDBD1.html
